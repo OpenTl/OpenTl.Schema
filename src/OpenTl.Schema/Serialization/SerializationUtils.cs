4706d9586f5e551d3f9679bb94714ec113ce3fa5
Виктор Борисов <viktor.borisov@bars-open.ru>
2017-07-07 23:43:49 +0700

    binary serialization was added

--- src/OpenTl.Schema/Serialization/SerializationUtils.cs
+++ src/OpenTl.Schema/Serialization/SerializationUtils.cs
@@ -13,5 +13,19 @@
         {
             return Encoding.UTF8.GetString(bytes);
         }
+        
+        public static byte[] GetBinaryFromString(string str)
+        {
+            var bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+        
+        public static string GetStringFromBinary(byte[] bytes)
+        {
+            var chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
     }
 }
