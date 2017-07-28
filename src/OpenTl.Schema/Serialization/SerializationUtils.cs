ec27251d1eb37517162516b5d47af427a65b9c77
Viktor Borisov <vik.borisov@outlook.com>
2017-07-28 19:06:12 +0700

    fix string serialization

--- src/OpenTl.Schema/Serialization/SerializationUtils.cs
+++ src/OpenTl.Schema/Serialization/SerializationUtils.cs
@@ -1,11 +1,14 @@
 ﻿namespace OpenTl.Schema.Serialization
 {
     using System;
+    using System.Diagnostics;
     using System.Linq;
     using System.Text;
 
     public class SerializationUtils
     {
+        private static readonly Encoding LatinEncoding = Encoding.GetEncoding("Latin1");
+
         public static byte[] GetBytes(string str)
         {
             return Encoding.UTF8.GetBytes(str);
@@ -15,17 +18,32 @@
         {
             return Encoding.UTF8.GetString(bytes);
         }
-        
+
         public static byte[] GetBinaryFromString(string str)
         {
-            return str.Select(Convert.ToByte).ToArray();
+            var bytes = LatinEncoding.GetBytes(str);
+
+            var decodedString = LatinEncoding.GetString(bytes);
+
+            Debug.Assert(str == decodedString);
+
+            return bytes;
         }
-        
+
         public static string GetStringFromBinary(byte[] bytes)
         {
-            var chars = bytes.Select(Convert.ToChar).ToArray();
-            
-            return new string(chars);
+            var str = LatinEncoding.GetString(bytes);
+
+            var decodedBytes = LatinEncoding.GetBytes(str);
+
+            Debug.Assert(bytes.Length == decodedBytes.Length);
+
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                Debug.Assert(bytes[i] == decodedBytes[i]);
+            }
+
+            return str;
         }
     }
 }
