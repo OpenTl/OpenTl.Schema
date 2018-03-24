f6b6a4be1e8c0919e18f6738e555b64485ea2dd4
Viktor Borisov <vik.borisov@outlook.com>
2018-03-24 13:01:43 +0700

    update docs; [skip ci]

--- build-docs.ps1
+++ build-docs.ps1
@@ -10,14 +10,18 @@ function Unzip
 
 Set-Location "docs_src"
 
-Invoke-WebRequest "https://raw.githubusercontent.com/Wyamio/Wyam/master/RELEASE" -OutFile ".\Wyam\wyamversion.txt"
-$version = Get-Content ".\Wyam\wyamversion.txt"
-$filePath = "https://github.com/Wyamio/Wyam/releases/download/$version/Wyam-$version.zip"
+if(!(Test-Path ".\Wyam")){
+    New-Item -ItemType directory -Path ".\Wyam"
+    Invoke-WebRequest "https://raw.githubusercontent.com/Wyamio/Wyam/master/RELEASE" -OutFile ".\Wyam\wyamversion.txt"
+    $version = Get-Content ".\Wyam\wyamversion.txt"
+    $filePath = "https://github.com/Wyamio/Wyam/releases/download/$version/Wyam-$version.zip"
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $filePath -OutFile ".\Wyam\Wyam.zip"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -Uri $filePath -OutFile ".\Wyam\Wyam.zip"
 
-Unzip ".\Wyam\Wyam.zip" ".\Wyam"
+    Unzip ".\Wyam\Wyam.zip" ".\Wyam"
+}
+
 
 &".\Wyam\wyam.exe" "--output" "..\docs"
 
